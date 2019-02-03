972db2da7734e3671f07d6788f100f549f5f79c5
Luke Davis <lucaswadedavis@gmail.com>
2019-02-02 19:15:28 -0800

    Add d3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Diagram from './Diagram';
 import './App.css';
 
+const data = [10, 2, 2, 34];
+
 class App extends Component {
+
+  componentDidMount() {
+    console.log('console mount');
+  }
+
+  componentDidUpdate() {
+    console.log('console update');
+  }
+
+  renderSvg() {
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Diagram data={ data } size={ [500, 500] }/>
       </div>
     );
   }
